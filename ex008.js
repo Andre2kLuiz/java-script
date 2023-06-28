641832bbeb7ab898e23494ef42e33e7b82cb2baf
André Luiz <123383093+Andre2kLuiz@users.noreply.github.com>
2023-06-28 16:59:18 -0300

    commit

---
+++ ex008.js
@@ -0,0 +1,11 @@
+
+
+var vel = 50
+
+console.log(`A velocidade do seu carro e: ${vel}Km/h`)
+if(vel >= 60) { // condição simples!
+    console.log(`Você ultarpassou a velocidade permitida. MULTADO!`)
+} else {
+    console.log(`Dirija smepre usando cintto de segurança!`)
+}
+
